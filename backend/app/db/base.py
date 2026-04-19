e21ba69308cdd347caa49be4dd6811954e473a54
Diyor-S <ruthlessfatality@gmail.com>
2026-04-19 21:53:11 +0500

    DBManager class for acting as the interface, let's see what comes out of it

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -1,5 +1,31 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+)
+
+from validators import AsyncURLValidator
+
+if TYPE_CHECKING:
+    from sqlalchemy import URL
+    from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class DBManager:
+    url: AsyncURLValidator = AsyncURLValidator()
+
+    def __init__(self, url: str) -> None:
+        self.url: "URL" = url
+        self.engine: "AsyncEngine" = create_async_engine(self.url)
+
+    async def session_factory(self):
+        pass
+
+    async def get_session(self):
+        pass
