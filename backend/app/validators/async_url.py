3953275497d73596c037cd92ff2e19e63ff74ae4
Diyor-S <ruthlessfatality@gmail.com>
2026-04-21 16:11:26 +0500

    improved and fixed some bugs of async url validator

--- backend/app/validators/async_url.py
+++ backend/app/validators/async_url.py
@@ -20,13 +20,23 @@ class AsyncURLValidator:
     def __get__(self, obj, obj_type=None) -> "URL":
         if obj is None:
             raise InstanceExpectedError(f"Need to be invoked via instance, got {obj_type.__name__}")
-        return getattr(obj, self.private_name)
+        
+        url_value = getattr(obj, self.private_name, None)
+
+        if url_value is None:
+            raise AttributeError("Attempt to access url before setting it!")
+        return url_value
 
     def __set__(self, obj, value) -> None:
         try:
             url_value = make_url(value)
-            dialect = url_value.drivername.split("+")[1]
-            if "async" not in dialect:
+            dialect = None
+            if "+" not in url_value.drivername:
+                dialect = url_value.drivername
+            else: 
+                dialect = url_value.drivername.split("+")[1]
+            
+            if "async" not in dialect and not dialect.startswith("aio"):
                 raise AsyncDialectExpected(f"Expected async dialect, but got {dialect}")
             setattr(obj, self.private_name, url_value)
         except ArgumentError:
