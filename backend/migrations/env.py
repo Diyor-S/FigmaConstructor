1912d5c6b19d6e8842add34822406b01fcc03b5c
Diyor-S <ruthlessfatality@gmail.com>
2026-04-24 21:55:07 +0500

    Added Base metadata config to alembic

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from backend.app.db import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -18,8 +20,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
-
+target_metadata = Base.metadata
+# TODO: Add db url to "sqlalchemy.url" param.
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
