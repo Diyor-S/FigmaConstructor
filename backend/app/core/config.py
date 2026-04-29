3e8ff38c7c71e602544ce004e6ee1673a4391219
Diyor-S <ruthlessfatality@gmail.com>
2026-04-29 23:39:25 +0500

    Added db configuration

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parents[2]
@@ -11,6 +11,14 @@ class Run(BaseModel):
     host: str = "127.0.0.1"
     port: int = 8000
 
+class DatabaseConfig(BaseModel):
+    """
+    params:
+        url - expected as `async` compatible, received via .env
+        metadata - configuration for alembic on naming convention
+    """
+    url: PostgresDsn
+    echo: bool = True
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
@@ -18,9 +26,10 @@ class Settings(BaseSettings):
         env_prefix="APP_CONFIG_",
         env_nested_delimiter="__",
         case_sensitive=False,
+        extra="allow"
     )
 
     run: Run = Field(default_factory=Run)
-
+    db: DatabaseConfig = Field(default_factory=DatabaseConfig)
 
 settings = Settings()
